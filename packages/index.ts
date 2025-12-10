8e0af6db89be8063f5196b524fdf8e473c41030d
heming <984507092@qq.com>
2025-12-10 14:13:06 +0800

    chore: prepare for v1.0.5 release

--- packages/index.ts
+++ packages/index.ts
@@ -5,13 +5,11 @@ interface InstallOptions {
   name?: string;
 }
 
-const install = function (app: App, options: InstallOptions = {}) {
-  app.component(options.name || Vue3AutoScroll.name || 'Vue3AutoScroll', Vue3AutoScroll);
+export default {
+  install: (app: App, options: InstallOptions = {}) => {
+    app.component(options.name || Vue3AutoScroll.name || 'Vue3AutoScroll', Vue3AutoScroll);
+  },
 };
-
-export default function (app: App) {
-  app.use(install as any);
-}
 
 export { Vue3AutoScroll };
 
